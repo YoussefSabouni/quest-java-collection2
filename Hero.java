8a688a549f0b1e14840dc1e524ffed358b575c40
Youssef Sabouni <youssefsabouni@icloud.com>
2020-08-30 02:43:56 +0200

    Resolution of the challenge

--- Hero.java
+++ Hero.java
@@ -1,4 +1,4 @@
-public class Hero {
+public class Hero implements Comparable<Hero>{
 
     // attributs
     private String name;
@@ -15,7 +15,7 @@ public class Hero {
         return name;
     }
 
-    public int getAge() {
+    public Integer getAge() {
         return age;
     }
 
@@ -26,5 +26,10 @@ public class Hero {
 
     public void setAge(int age) {
         this.age = age;
+    }
+
+    @Override
+    public int compareTo(Hero hero) {
+        return this.getName().compareTo(hero.getName());
     }
 }
